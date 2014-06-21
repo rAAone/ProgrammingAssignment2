830d60185992583ec674641786f6926abeae905d
rAAone <rakesh.undavalli@gmail.com>
2014-06-21 18:11:12 +0530

    final solution
    the functions have been defined

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## This function takes in a vector as argument and transforms it into a square matrix
+## make sure that the length of the input is a perfect square so as to get a square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    dim(y)<-c(sqrt(length(y)),sqrt(length(y)))
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function returns the cached inverse or calculates and then returns the inverse of a square matrix if an inverse of the matrix exists
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
 }
